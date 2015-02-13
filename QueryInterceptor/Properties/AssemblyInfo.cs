3fe1ad819dccea287e73fc304edba7b3a8440b22
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2015-02-13 18:05:11 +0100

    Added xUnitTest project
    Added xUnitTest project

--- QueryInterceptor/Properties/AssemblyInfo.cs
+++ QueryInterceptor/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("David Fowler, Stef Heyenrath")]
 [assembly: AssemblyProduct("QueryInterceptorAsync")]
-[assembly: AssemblyCopyright("Copyright © David Fowler 2011, Stef Heyenrath 2014")]
+[assembly: AssemblyCopyright("Copyright © David Fowler 2011, Stef Heyenrath 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
---
+++ QueryInterceptorAsyncUnitTests/Test.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading;
+using QueryInterceptor;
+using Xunit;
+
+namespace QueryInterceptorAsyncUnitTests
+{
+    public class EqualsToNotEqualsVisitor : ExpressionVisitor
+    {
+        protected override Expression VisitBinary(BinaryExpression node)
+        {
+            if (node.NodeType == ExpressionType.Equal)
+            {
+                // Change == to !=
+                return Expression.NotEqual(node.Left, node.Right);
+            }
+
+            return base.VisitBinary(node);
+        }
+    }
+
+    public class Test
+    {
+        [Fact]
+        public void TestEqualsToNotEqualsVisitor()
+        {
+            var query = Enumerable.Range(0, 10).AsQueryable().Where(n => n%2 == 0);
+            var numbersEven = query.ToList();
+            Assert.Equal(new List<int> {0, 2, 4, 6, 8}, numbersEven);
+
+            var visitor = new EqualsToNotEqualsVisitor();
+            var numbersOdd = query.InterceptWith(visitor).Where(x => x >= 0).ToList();
+            Assert.Equal(new List<int> {1, 3, 5, 7, 9}, numbersOdd);
+        }
+
+        [Fact]
+        public void TestAsync()
+        {
+            var queryEven = Enumerable.Range(0, 10).AsQueryable().Where(n => n%2 == 0);
+
+            var visitor = new EqualsToNotEqualsVisitor();
+            var queryOdd = queryEven.InterceptWith(visitor);
+
+            var task = queryOdd.FirstAsync(n => n > 5, CancellationToken.None);
+            task.ContinueWith(t => t.Result).Wait();
+
+            Assert.Equal(7, task.Result);
+        }
+    }
+}
