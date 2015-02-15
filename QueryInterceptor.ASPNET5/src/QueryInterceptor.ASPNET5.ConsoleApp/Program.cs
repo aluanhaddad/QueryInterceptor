23ace4ed80ad8e99c222c1ada24ef87422f1d7c2
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2015-02-15 16:32:06 +0100

    Added ConsoleApp
    Added ConsoleApp

---
+++ QueryInterceptor.ASPNET5/src/QueryInterceptor.ASPNET5.ConsoleApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace QueryInterceptor.ASPNET5.ConsoleApp
+{
+    public class EqualsToNotEqualsVisitor : ExpressionVisitor
+    {
+        protected override Expression VisitBinary(BinaryExpression node)
+        {
+            if (node.NodeType == ExpressionType.Equal)
+            {
+                // Change == to !=
+                return Expression.NotEqual(node.Left, node.Right);
+            }
+
+            return base.VisitBinary(node);
+        }
+    }
+
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+            Console.WriteLine("Hello QueryInterceptor.ASPNET5");
+
+            IQueryable<int> query = Enumerable.Range(0, 10).AsQueryable().Where(n => n > 0 && n % 2 == 0);
+
+            List<int> numbersEven = query.ToList();
+            Console.WriteLine("numbersEven > 0 = {0}", string.Join(", ", numbersEven));
+
+            var visitor = new EqualsToNotEqualsVisitor();
+            List<int> numbersOdd = query.InterceptWith(visitor).Where(x => x >= 0).ToList();
+            Console.WriteLine("numbersOdd  > 0 = {0}", string.Join(", ", numbersOdd));
+
+            Console.ReadLine();
+        }
+    }
+}
